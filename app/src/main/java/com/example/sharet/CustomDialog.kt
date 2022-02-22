2ffc9d2e19e670bf7ed9152a6454d1537ce56683
bruzzese95 <bruzzese.1648786@studenti.uniroma1.it>
2022-02-22 19:40:08 +0100

    navigation implemented on CustomDialog with some bugs

--- app/src/main/java/com/example/sharet/CustomDialog.kt
+++ app/src/main/java/com/example/sharet/CustomDialog.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.sharet.databinding.CustomDialogAddResourceBinding
 
 class CustomDialog: DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: CustomDialogAddResourceBinding = DataBindingUtil.inflate(
+            inflater, R.layout.custom_dialog_add_resource, container, false)
+        binding.submitCreateResourceButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_customDialog_to_sharedResourceFragment))
         getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.round_corner);
-        return inflater.inflate(R.layout.custom_dialog_add_resource, container, false)
+        return binding.root
     }
 
     override fun onStart() {
