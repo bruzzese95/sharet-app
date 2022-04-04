97d67b18fa5416ed9cf8f2b9b133fc72e22daa58
bruzzese95 <bruzzese.1648786@studenti.uniroma1.it>
2022-04-04 12:42:53 +0200

    network classes implemented

--- app/src/main/java/it/sapienza/macc/sharet/network/SharedResourceProperty.kt
+++ app/src/main/java/it/sapienza/macc/sharet/network/SharedResourceProperty.kt
@@ -2,5 +2,7 @@ package it.sapienza.macc.sharet.network
 
 data class SharedResourceProperty (
             val id: String,
-            val name: String
+            val name: String,
+            val owner: UserProperty,
+            val group: List<UserProperty>
 )
---
+++ app/src/main/java/it/sapienza/macc/sharet/network/UserProperty.kt
@@ -0,0 +1,7 @@
+package it.sapienza.macc.sharet.network
+
+data class UserProperty(
+        val id: String,
+        val name: String,
+        val email: String
+)
